7b4a77a5d027ee25ff3dd02340d7c3b8e6d19c3f
JossylynCasseb <157176311+JossylynCasseb@users.noreply.github.com>
2024-07-16 19:45:57 -0300

    Update SW.js
    reescrito

--- SW.js
+++ SW.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = 'pwa-cache-v1';
 const urlsToCache = [
     '/',
-    'index.html',
-    'style.css',
-    'app.js'
+    '/index.html',
+    '/style.css',
+    '/app.js'
 ];
 
 self.addEventListener('install', function(event) {
